eb8d24a974a9e26153b15014c26b0c93f92da470
kaylaadilafa <kaylaadila@mhs.usk.ac.id>
2025-11-13 14:47:31 +0700

    Create file.h
    Added header file with data structures and function declarations for the laboratory equipment borrowing system.

---
+++ Sistem Peminjaman Alat Laboratorium/file.h
@@ -0,0 +1,35 @@
+#ifndef FILE_H  // Header utama yang berisi struktur data dan deklarasi fungsi
+#define FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Struktur akun
+typedef struct{
+    char username[50];
+    char password[50];
+    char role[10];
+}Akun;
+
+// Struktur alat laboratorium
+typedef struct{
+    unsigned int Id_Alat;
+    char Nama_Alat[50];
+    char Merek_Alat[50];
+    char Model_Alat[50];
+    unsigned int Tahun_Produksi;
+    unsigned int Jumlah_unit;
+}item;
+
+// Deklarasi fungsi umum
+void garis();
+
+// Deklarasi fungsi login
+int login(const char *username, const char *password, char *role);
+
+// Deklarasi fungsi admin
+void tampilAlat();
+void tambahAlat();
+void editAlat();
+void hapusAlat();
