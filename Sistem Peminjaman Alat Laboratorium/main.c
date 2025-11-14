4b6a6bdc38a0f9af77e7e680ba1ec6b8f9cf37a7
Naila Syakira Bahri <NAILA.SB@mhs.usk.ac.id>
2025-11-14 20:29:11 +0700

    Update main.c

--- Sistem Peminjaman Alat Laboratorium/main.c
+++ Sistem Peminjaman Alat Laboratorium/main.c
@@ -27,3 +27,35 @@ void menuAdmin(){
             case 4: hapusAlat(); 
             break;
             case 5:
+            printf("Keluar dari menu admin...\n");
+            break;
+            default:
+            printf("Pilihan tidak valid!\n");
+        }
+    }while(pilih != 5);
+}
+
+// Fungsi menu untuk user
+void menuUser(const char *username){ // Menerima username sebagai parameter
+    int pilihan;
+    do{
+        garis();
+        printf("MENU USER LAB INFORMATIKA\n");
+        garis();
+        printf("1. Lihat daftar alat\n");
+        printf("2. Pinjam alat\n");
+        printf("3. Kembalikan alat\n");
+        printf("4. Lihat alat yang sedang dipinjam\n");
+        printf("5. Keluar\n");
+        garis();
+        printf("Pilih menu: ");
+        scanf("%d", &pilihan);
+        switch(pilihan){
+            case 1:
+            totalalat();
+            break;
+            case 2:
+            minjamalat(username);
+            break;
+            case 3:
+            balikinalat(username);
